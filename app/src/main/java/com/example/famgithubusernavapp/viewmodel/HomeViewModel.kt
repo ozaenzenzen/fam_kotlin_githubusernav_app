00ebeaa1ed9e4e1b828049f1b8a4f807ea3c6e16
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2024-04-07 22:43:26 +0700

    upate home view model

--- app/src/main/java/com/example/famgithubusernavapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/famgithubusernavapp/viewmodel/HomeViewModel.kt
@@ -1,23 +1,31 @@
 package com.example.famgithubusernavapp.viewmodel
 
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dicoding.picodiploma.githubuseruiuxapi.model.User
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel : ViewModel() {
     companion object {
         private val TAG = HomeViewModel::class.java.simpleName
-//        private const val GITHUB_API = BuildConfig.GithubAPI
+
+        //        private const val GITHUB_API = BuildConfig.GithubAPI
         private const val GITHUB_API = "7e5f26491b034e054fe1cded505a3ebbaa024978"
 //        const val GithubAPI = "7e5f26491b034e054fe1cded505a3ebbaa024978"
     }
 
     private val listUsername = MutableLiveData<ArrayList<User>>()
-    private lateinit var errorMessage:String
+    private lateinit var errorMessage: String
     fun setUsername(username: String, context: Context) {
         val listItem = ArrayList<User>()
         val searchEndpoint = "https://api.github.com/search/users?q=$username"
@@ -25,13 +33,30 @@ class HomeViewModel: ViewModel() {
 
         client.addHeader("Authorization", "token $GITHUB_API")
         client.addHeader("User-Agent", "request")
-        client.get(searchEndpoint, object: AsyncHttpResponseHandler(){
+        client.get(searchEndpoint, object : AsyncHttpResponseHandler() {
             override fun onSuccess(
                 statusCode: Int,
                 headers: Array<out Header>?,
                 responseBody: ByteArray?
             ) {
-                TODO("Not yet implemented")
+                try {
+                    val result = responseBody?.let { String(it) }
+                    Log.d(TAG, result!!)
+                    val responseObject = JSONObject(result)
+                    val items = responseObject.getJSONArray("items")
+                    for (i in 0 until items.length()) {
+                        val item = items.getJSONObject(i)
+                        val dataAvatar = item.getString("avatar_url")
+                        val dataUsername = item.getString("login")
+                        val dataId = item.getInt("id")
+                        val dataType = item.getString("type")
+                        val user = User(dataAvatar, dataUsername, dataId, dataType)
+                        listItem.add(user)
+                    }
+                    listUsername.postValue(listItem)
+                } catch (e: Exception) {
+                    Log.d(TAG, e.message.toString())
+                }
             }
 
             override fun onFailure(
@@ -40,13 +65,20 @@ class HomeViewModel: ViewModel() {
                 responseBody: ByteArray?,
                 error: Throwable?
             ) {
-                TODO("Not yet implemented")
+                errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Log.d(TAG, errorMessage)
+                GlobalScope.launch(Dispatchers.Main) {
+                    Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+                }
             }
 
         })
     }
 
-    fun getUsername(){
-
-    }
+    fun getUsername(): LiveData<ArrayList<User>> = listUsername
 }
